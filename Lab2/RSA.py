65f8090b89409f2c8bb4ee4e2c2f4907ffd525d4
stepan <step.rus.f@gmail.com>
2023-03-02 13:06:40 +0300

    lab2

--- Lab2/RSA.py
+++ Lab2/RSA.py
@@ -3,18 +3,18 @@ from Ciphergram import Cipher
 
 
 class RSA():
-    def __init__(self, p: int, q: int, e:int,word: str) -> None:
-        self.word = word
+    def __init__(self, p: int, q: int, e: int, word: str) -> None:
+        self.word = word.upper()
         self.p = p
         self.q = q
         self.e = e
 
-    def GetOpenKey(self) -> tuple:
+    def get_open_key(self) -> tuple:
         n = self.p * self.q
         open_key = (self.e, n)
         return open_key
 
-    def GetPrivateKey(self) -> tuple:
+    def get_private_key(self) -> tuple:
         f_n = (self.p - 1) * (self.q - 1)
         n = self.p * self.q
         k = 0
@@ -26,24 +26,19 @@ class RSA():
         private_key = (int(d), n)
         return private_key
 
-    def Encode(self) -> list:
+    def encode(self) -> list:
         word_list = list(self.word)
         encode_word = []
         for i in word_list:
-            c_i = Cipher[i] ** self.GetOpenKey()[0] % self.GetOpenKey()[1]
+            c_i = Cipher[i] ** self.get_open_key()[0] % self.get_open_key()[1]
             encode_word.append(c_i)
         return encode_word
 
-    def Decode(self) -> None:
+    def decode(self) -> None:
         decode_index = []
-        for i in self.Encode():
-            t_i = i ** self.GetPrivateKey()[0] % self.GetPrivateKey()[1]
+        for i in self.encode():
+            t_i = i ** self.get_private_key()[0] % self.get_private_key()[1]
             decode_index.append(t_i)
         for i in decode_index:
             letters = [k for k, v in Cipher.items() if v == i]
             print(f"{''.join(letters)} - {i}")
-
-
-
-
-
--- Lab2/main.py
+++ Lab2/main.py
@@ -1,6 +1,6 @@
 from RSA import RSA
 
 rsa = RSA(7, 13,11, "АФФФС")
-print(rsa.Encode())
-rsa.Decode()
+print(rsa.encode())
+rsa.decode()
 
